f434913ef1a9881f9336f1ed519fcad368a2dbdb
pezzaliapp <pezzalialessandro@gmail.com>
2025-06-27 22:25:27 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDMlSihgBH4I2IVBbNqhT1-lYWFgEPd08Q",
+  authDomain: "ferryguard-bc3fc.firebaseapp.com",
+  projectId: "ferryguard-bc3fc",
+  storageBucket: "ferryguard-bc3fc.firebasestorage.app",
+  messagingSenderId: "555623410470",
+  appId: "1:555623410470:web:cac2530276035ffd906fe7"
+};
